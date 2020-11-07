be2ed3a739c572138f89ffc5e383ae4abd7c603e
soniasingla <soniasingla.1812@gmail.com>
2020-11-07 21:15:30 +0530

    Blog setup

---
+++ config/index.js
@@ -0,0 +1,24 @@
+const site = {
+  title: 'Personal Blog',
+  author: 'Sonia Singla',
+  description: 'I write, speak, and code about my work.',
+  siteUrl: 'https://soniasingla.com',
+  twitter: 'soniasinglas',
+  github: 'soniasingla',
+  medium: 'soniasingla',
+  facebook: 'soniasinglas',
+  disqusShortName: 'gatsby-simple-blog',
+  googleTrackingId: '167005401',
+  lang: 'en',
+  displayTranslations: true,
+  postsPerPage: 3,
+};
+
+const supportedLanguages = {
+  en: 'English',
+};
+
+module.exports = {
+  site,
+  supportedLanguages,
+};
